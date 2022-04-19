1ea6844885a41224d6c51a0e66f4897de2318802
Raghad <raghadsalameh926@gmail.com>
2022-04-19 08:35:27 +0300

    Ideal removed

--- IdeaProjects/Testjavafx/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.testjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.testjavafx to javafx.fxml;
-    exports com.example.testjavafx;
-}
--- IdeaProjects/demo1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.demo1 {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.demo1 to javafx.fxml;
-    exports com.example.demo1;
-}
