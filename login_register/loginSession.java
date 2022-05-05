53408a123ec65e8e0f7a624e319315214eafc602
raghaadd <100478249+raghaadd@users.noreply.github.com>
2022-05-05 21:19:05 +0300

    Add files via upload

---
+++ login_register/loginSession.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package login_register;
+
+/**
+ *
+ * @author Sara
+ */
+public class loginSession {
+   // public static int UID;
+    public static String UserType;
+    public static boolean isLogged= false;
+}
